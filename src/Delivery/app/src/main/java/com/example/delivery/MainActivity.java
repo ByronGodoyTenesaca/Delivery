9c801ae2e6dbe46e4ffbe9ec217495cb12f69917
Byron Godoy <bgodoyt@est.ups.edu.ec>
2020-05-14 18:48:58 -0500

    comienzo del proyecto

---
+++ src/Delivery/app/src/main/java/com/example/delivery/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.delivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void pasar(View vi){
+        Intent i=new Intent(this,Principal.class);
+        startActivity(i);
+
+    }
+}
